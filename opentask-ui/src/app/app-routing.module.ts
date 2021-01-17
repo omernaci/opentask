5391751c3aa1238c7cf3692e3c1e656456e7b4b3
erdem <erdem.sen@softtech.com.tr>
2021-01-17 23:17:43 +0300

    Make routes safe with route guard service.

--- opentask-ui/src/app/app-routing.module.ts
+++ opentask-ui/src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { CreateTaskComponent } from './components/create-task/create-task.component';
 import { TaskListComponent } from './components/task-list/task-list.component';
 import { LoginComponent } from './login/login.component';
+import { RouteGuardService } from './services/route-guard.service';
 
 const routes: Routes = [
   {path:'', component:LoginComponent},//default page is login
   {path:'login', component:LoginComponent},
-  {path:'**', component:TaskListComponent},//go to task list component when an invalid page requested
-  {path:'tasks', component:TaskListComponent},
-  {path:'create-task', component:CreateTaskComponent}
+  {path:'tasks', component:TaskListComponent, canActivate:[RouteGuardService]},
+  {path:'create-task', component:CreateTaskComponent, canActivate:[RouteGuardService]},
+  {path:'**', component:TaskListComponent}//go to task list component when an invalid page requested
 ];
 
 @NgModule({
