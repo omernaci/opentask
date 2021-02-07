1df8d434ab2704adce087f2c170c7e58bb55cd68
erdem <erdem.sen@softtech.com.tr>
2021-02-07 15:38:41 +0300

    CRUD methods for task service.

--- opentask-ui/src/app/services/task.service.ts
+++ opentask-ui/src/app/services/task.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { AuthenticationService } from './authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private authService: AuthenticationService) { }
+
+  getTaskList(){
+    return this.http.get<any>(`${environment.apiUrl}/api/v1/tasks`);
+  }
+
+  updateTask(task){
+    return this.http.put<any>(environment.apiUrl,{params : task})
+  }
+
+  deleteTask(id){
+    return this.http.delete(`${environment.apiUrl}/api/v1/tasks/${id}`);
+  }
 }
